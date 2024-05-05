6d4015cacf7fb834797d9bcc6fd5f140265b8177
NANAQS <otaviotokyo5@gmail.com>
2024-05-05 19:03:55 -0300

    feat: add mentorias and curso class

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import domain.Curso;
+import domain.Mentorias;
+
+public class App {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setCargaHoraria(199);
+        curso1.setDescricao("curso basico");
+        curso1.setTitulo("Javascript");
+
+        Curso curso2 = new Curso();
+        curso2.setCargaHoraria(299);
+        curso2.setDescricao("curso basico");
+        curso2.setTitulo("PHP");
+
+        Mentorias mentoria1 = new Mentorias();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("mentoria");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1.toString());
+    }
+}
